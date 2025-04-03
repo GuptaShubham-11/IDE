7c982b2d0ade4c09035f98d212eea277f31cc79e
GuptaShubham-11 <shu919847@gmail.com>
2025-04-03 15:22:41 +0530

    add User model with authentication methods and password hashing

---
+++ server/src/models/user.ts
@@ -0,0 +1,81 @@
+import { Schema, Document, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt, { Secret } from "jsonwebtoken";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+    createdAt: Date;
+    updatedAt: Date;
+    generateAccessToken(): string;
+    generateRefreshToken(): string;
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+            maxlength: 12
+        },
+        avatar: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET as Secret,
+        {
+            expiresIn: Number(process.env.ACCESS_TOKEN_EXPIRY),
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET as Secret,
+        {
+            expiresIn: Number(process.env.REFRESH_TOKEN_EXPIRY),
+        }
+    );
+};
+
+const User = model<IUser>("User", userSchema);
+export default User;
